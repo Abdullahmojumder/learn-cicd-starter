7375ff780cb0450f41c53ae513cc9fff15403ff5
abdullah <abdullah.mojumder@gmail.com>
2025-04-03 15:41:32 +0600

    Fix all

--- json.go
+++ json.go
@@ -21,13 +21,13 @@ func respondWithError(w http.ResponseWriter, code int, msg string, logErr error)
 	}
 }
 
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
-		return
+		return err
 	}
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
